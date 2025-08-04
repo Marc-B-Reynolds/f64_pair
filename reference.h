8e32b899b57b4a61242cf700eeb8523f7248e450
Marc B. Reynolds <Marc-B-Reynolds@users.noreply.github.com>
2025-08-04 14:21:59 +0200

    Update reference.h

--- reference.h
+++ reference.h
@@ -3,19 +3,12 @@
 // paper or other reference-y versions of routines that are modified
 // in f64_pair.h
 
-static inline int paper_not_1or3_time_pot(double x)
-{
-  static const double P = 2251799813685249.0, Q = 2251799813685248.0;
-  double Delta;
-  Delta = (P*x)-(Q*x);
-  return (Delta != x);
-}
 
-
-static inline double sum_ro_f64_(double x, double y)
+// Boldo & Melquiond (2008) round-to-odd
+static inline double bm_sum_ro(double x, double y)
 {
   fe_pair_t v = fe_two_sum(x,y);
-  uint64_t  r = type_pun(v.hi,uint64_t);
+  uint64_t  r = fe_to_bits(v.hi);
 
   // expectation: probability of being odd/even is
   // equal and exact is about zero.  sadface for
@@ -27,6 +20,41 @@ static inline double sum_ro_f64_(double x, double y)
       r--;
   }
 
-  return type_pun(r,double);
+  return fe_from_bits(r);
 }
 
+
+// Boldo & Melquiond (2008) round-to-odd
+static inline double bm_add3(double a, double b, double c)
+{
+  fe_pair_t x = fe_two_sum(a,b);
+  fe_pair_t s = fe_two_sum(c,x.hi);
+  double    v = bm_sum_ro(x.lo,s.lo);
+
+  return(s.hi+v);
+}
+
+// Graillat & Muller (2025)
+static inline int gm_not_1or3_times_pot(double x)
+{
+  static const double P = 2251799813685249.0, Q = 2251799813685248.0;
+  double Delta;
+  Delta = (P*x)-(Q*x);
+  return (Delta != x);
+}
+
+// Graillat & Muller (2025)
+double gm_add3(double a, double b, double c)
+{
+  fe_pair_t x = fe_two_sum(a,b);
+  fe_pair_t s = fe_two_sum(x.hi,c);
+  fe_pair_t v = fe_two_sum(x.lo,s.lo);
+
+  if ((gm_not_1or3_times_pot(v.hi)) || (v.lo == 0))
+    return s.hi+v.hi;
+
+  if ((v.lo < 0) ^ (v.hi < 0))
+    return s.hi+(0.875*v.hi);
+  else
+    return s.hi+(1.125*v.hi);
+}
