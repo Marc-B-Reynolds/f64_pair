306b5072493909035ba86388aa43316926ec1ec0
Marc B. Reynolds <Marc-B-Reynolds@users.noreply.github.com>
2025-08-08 19:39:19 +0200

    Update common.h

--- test/common.h
+++ test/common.h
@@ -17,8 +17,16 @@
 #define  PRNG_IMPLEMENTATION
 #include "prng_small_global.h"
 
+#include <mpfr.h>
+
 #define LENGTHOF(X) (sizeof(X)/sizeof(X[0]))
 
+#define DEF_FE(N,M) .fe=N,.name=#N,.mp=&M
+#define DEF_FR(N,M) .fr=N,.name=#N,.mp=&M
+#define OP_U(U,X,R) .max_ulp=U,.max_x=X,.max_r=R
+#define OP_B(U,A,B,R) .max_ulp=U,.max_a=A,.max_b=B,.max_r=R
+
+#define UP(H,L) {.hi=H,.lo=L}
 
 static const uint64_t f64_one_bits_k = UINT64_C(0x3ff0000000000000);
 
@@ -52,4 +60,43 @@ static inline fe_pair_t prng_fe(void)
   lo = type_pun(type_pun(lo,uint64_t) ^ u, double);
 
   return fe_fast_sum(hi,lo);
+}
+
+
+static inline void mp_set(mpfr_t r, fe_pair_t x)
+{
+  mpfr_set_d(r,  x.hi, MPFR_RNDN);
+  mpfr_add_d(r,r,x.lo, MPFR_RNDN);
+};
+
+extern mpfr_t mp_t;
+
+static fe_pair_t mp2fe(mpfr_t r)
+{
+  double hi = mpfr_get_d(r,MPFR_RNDN);
+
+  mpfr_set_d(mp_t,hi,   MPFR_RNDN);
+  mpfr_sub(mp_t,r,mp_t, MPFR_RNDN);
+  
+  double lo = mpfr_get_d(mp_t,MPFR_RNDN);
+  
+  return fe_pair(hi,lo);
+};
+
+// WARNING: modifies global 'mp_e' and passed in 'mp_t'
+// assumes finite and normal
+extern mpfr_t mp_e;
+
+double ulp_dist(mpfr_t mp_r, fe_pair_t r)
+{
+  mpfr_exp_t rexp = mpfr_get_exp(mp_r);
+
+  mp_set(mp_e,r);
+
+  mpfr_set(mp_t,mp_r,MPFR_RNDN);
+  mpfr_sub(mp_t,mp_t,mp_e,      MPFR_RNDN);
+  mpfr_abs(mp_t,mp_t,           MPFR_RNDN);
+  mpfr_mul_2si(mp_t,mp_t,53+54-rexp, MPFR_RNDN);
+  
+  return mpfr_get_d(mp_t, MPFR_RNDU);
 }
